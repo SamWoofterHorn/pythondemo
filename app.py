e2f3796f5ca1bf83cbcacda9e984538ea3d42330
SamWoofterHorn <shorn@altalang.com>
2023-06-29 15:40:26 -0500

    going back

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def server():
     from pythondemo import create_app
 
     app = create_app()
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5002)
 
 
 if __name__ == "__main__":
